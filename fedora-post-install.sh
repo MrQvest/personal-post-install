ac2482f5f7fb29efcba9d7577a241a30a7e44a7a
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-05-15 21:38:33 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo dnf update -y && flatpak update -y --noninteractive;
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y; 
 sudo dnf groupupdate core -y;
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y; 
@@ -11,8 +12,7 @@ sudo dnf install rpmfusion-free-release-tainted -y;
 sudo dnf install libdvdcss -y; 
 sudo dnf install rpmfusion-nonfree-release-tainted -y;
 sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y; 
-sudo dnf install gnome-tweaks easyeffects ffmpegthumbnailer steam-devices yaru-theme neovim yt-dlp -y; 
-sudo dnf remove firefox -y; 
-flatpak install -y --noninteractive flathub com.spotify.Client com.valvesoftware.Steam org.videolan.VLC io.github.shiftey.Desktop com.heroicgameslauncher.hgl org.prismlauncher.PrismLauncher com.unity.UnityHub org.keepassxc.KeePassXC com.visualstudio.code com.github.micahflee.torbrowser-launcher org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal org.qbittorrent.qBittorrent io.github.spacingbat3.webcord org.mozilla.Thunderbird; 
-sudo dnf update -y && flatpak update -y --noninteractive;
+sudo dnf install gnome-tweaks easyeffects ffmpegthumbnailer steam-devices neovim yt-dlp -y;
+sudo dnf remove firefox -y;
 sudo dnf install akmod-nvidia -y;
+flatpak install -y --noninteractive flathub com.spotify.Client com.valvesoftware.Steam org.videolan.VLC io.github.shiftey.Desktop com.heroicgameslauncher.hgl org.prismlauncher.PrismLauncher com.unity.UnityHub org.keepassxc.KeePassXC com.visualstudio.code com.github.micahflee.torbrowser-launcher org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal org.qbittorrent.qBittorrent io.github.spacingbat3.webcord org.mozilla.Thunderbird; 
