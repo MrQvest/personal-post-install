eb996a3a6344edc3213bf58c009eeae160ae6b5a
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-05-18 11:51:53 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
+
+# Update the system
 sudo dnf update -y &&
+
+# Install RPM Fusion repositories and Multimedia stuff / codecs
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y && 
 sudo dnf groupupdate core -y &&
 sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y && 
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y &&
 sudo dnf groupupdate sound-and-video -y &&
+
+# Install hardware accelerated codecs
 sudo dnf install intel-media-driver -y &&
 sudo dnf install libva-intel-driver -y &&
 sudo dnf install nvidia-vaapi-driver -y &&
+
+# DVD
 sudo dnf install rpmfusion-free-release-tainted -y &&
 sudo dnf install libdvdcss -y &&
+
+# Firmware
 sudo dnf install rpmfusion-nonfree-release-tainted -y &&
-sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y && 
+sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y &&
+
+# Personal stuff
 sudo dnf install gnome-tweaks easyeffects ffmpegthumbnailer steam-devices neovim yt-dlp -y &&
+
+# NVIDIA Driver
 sudo dnf install akmod-nvidia -y &&
+
+# More personal stuff
 sudo dnf remove firefox totem -y &&
 
+# Install personal flatpak collection
 flatpak install -y --noninteractive flathub com.spotify.Client com.valvesoftware.Steam org.videolan.VLC io.github.shiftey.Desktop com.heroicgameslauncher.hgl org.prismlauncher.PrismLauncher com.unity.UnityHub org.keepassxc.KeePassXC com.visualstudio.code com.github.micahflee.torbrowser-launcher org.mozilla.firefox com.mattjakeman.ExtensionManager com.github.tchx84.Flatseal org.qbittorrent.qBittorrent io.github.spacingbat3.webcord org.mozilla.Thunderbird 
