2ba5aaa7a7b095d7090cdcd4d11d42d99a11e2df
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-07-28 17:59:41 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -5,6 +5,7 @@
 # sudo grubby --remove-args="preempt=full" --update-kernel=ALL
 sudo grubby --args="preempt=full" --update-kernel=ALL
 
+# RPMFusion stuff
 # Install RPM Fusion repositories 
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y 
 
