9aa86f1b0306f878ef8d6fd38ce93c7520595a73
MrQvest <104365438+MrQvest@users.noreply.github.com>
2023-08-23 14:42:31 -0300

    remove deltaRPMs

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -8,7 +8,6 @@
 # dnf configuration
 echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
-echo 'deltarpm=True' | sudo tee -a /etc/dnf/dnf.conf
 
 # GDM configuration
 sudo touch /etc/dconf/db/gdm.d/06-tap-to-click
