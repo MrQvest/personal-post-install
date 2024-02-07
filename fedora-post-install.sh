65696abb8863a0f0b3f334452eef493e797b9e94
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-02-07 12:04:33 -0300

    add neofetch

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -54,7 +54,7 @@ sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
 sudo dnf install heif-pixbuf-loader libheif-tools -y
 
 # Utilities
-sudo dnf install transmission grsync neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-drive-menu gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts -y
+sudo dnf install transmission grsync neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-drive-menu gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch -y
 
 # Essentials
 sudo dnf install strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp papirus-icon-theme -y
