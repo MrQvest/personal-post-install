eeaaf506636064720a71c8c49a09b6a47fdedca2
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-02-14 18:01:52 -0300

    Add zoxide

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -57,7 +57,7 @@ sudo dnf install heif-pixbuf-loader libheif-tools -y
 sudo dnf install grsync neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-drive-menu gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop -y
 
 # Essentials
-sudo dnf install strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp zsh zsh-autosuggestions zsh-syntax-highlighting gh papirus-icon-theme -y
+sudo dnf install strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp zsh zsh-autosuggestions zsh-syntax-highlighting gh papirus-icon-theme zoxide -y
 
 # Not needed apps
 sudo dnf remove libreoffice-core rhythmbox totem gnome-extensions-app simple-scan mediawriter gnome-connections gnome-clocks gnome-contacts gnome-weather gnome-text-editor gnome-calculator gnome-tour gnome-system-monitor gnome-shell-extension-gamemode gnome-classic-session gnome-font-viewer gnome-characters loupe gnome-calendar cheese gnome-maps -y
