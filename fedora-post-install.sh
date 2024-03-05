5b9d7030affe12d5226d64f8ea8a61455de66548
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-03-05 13:28:52 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -45,14 +45,8 @@ sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
 
 # Personal stuff
 
-# .heic extension support (for Apple photos)
-sudo dnf install heif-pixbuf-loader libheif-tools -y
-
-# Utilities
-sudo dnf install restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-drive-menu gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop -y
-
-# Essentials
-sudo dnf install strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme -y
+# .heic extension support (for Apple photos), Utilities, Essetials
+sudo dnf install heif-pixbuf-loader libheif-tools restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme -y
 
 # Not needed apps
 sudo dnf remove libreoffice-core rhythmbox totem gnome-extensions-app simple-scan mediawriter gnome-connections gnome-clocks gnome-contacts gnome-weather gnome-text-editor gnome-calculator gnome-tour gnome-system-monitor gnome-shell-extension-gamemode gnome-classic-session gnome-font-viewer gnome-characters loupe gnome-calendar cheese gnome-maps firefox -y
