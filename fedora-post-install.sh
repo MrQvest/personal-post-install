bd68f9f77635048e4f40bf767ae2acefb9bace5b
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-03-10 14:04:39 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# dotfiles configuration
+git clone --bare https://github.com/MrQvest/.dotfiles.git $HOME/.dotfiles
+alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+dotfiles checkout -f
+dotfiles config --local status.showUntrackedFiles no
+
+# Install Starship Prompt
+curl -sS https://starship.rs/install.sh | sh
+
 # dnf configuration
 echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
@@ -45,25 +54,12 @@ sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
 
 # Personal stuff
 
-# .heic extension support (for Apple photos), Utilities, Essetials
-sudo dnf install heif-pixbuf-loader libheif-tools restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop strawberry steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme gnome-shell-theme-yaru yaru-sound-theme -y
+# .heic extension support (for Apple photos), Utilities, Essetials, NVIDIA Drivers
+sudo dnf install heif-pixbuf-loader libheif-tools restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop strawberry steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme gnome-shell-theme-yaru yaru-sound-theme solaar akmod-nvidia xorg-x11-drv-nvidia-cuda -y
 
 # Not needed apps
 sudo dnf remove libreoffice-core rhythmbox totem gnome-extensions-app simple-scan mediawriter gnome-connections gnome-clocks gnome-contacts gnome-weather gnome-text-editor gnome-calculator gnome-tour gnome-system-monitor gnome-shell-extension-gamemode gnome-classic-session gnome-font-viewer gnome-characters loupe gnome-calendar cheese gnome-maps firefox -y
 sudo dnf group remove Libreoffice -y
-
-# dotfiles configuration
-git clone --bare https://github.com/MrQvest/.dotfiles.git $HOME/.dotfiles
-alias dotfiles='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
-dotfiles checkout -f
-dotfiles config --local status.showUntrackedFiles no
-
-# Install Starship Prompt
-curl -sS https://starship.rs/install.sh | sh
-
-# NVIDIA Drivers
-sudo dnf install akmod-nvidia -y
-sudo dnf install xorg-x11-drv-nvidia-cuda -y
 
 # Gnome Shell extensions
 # Espresso
