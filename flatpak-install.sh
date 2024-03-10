5cc032dea32b1929150e994d20da4db321f53534
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-03-10 13:13:10 -0300

    Create flatpak-install.sh

---
+++ flatpak-install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Install personal flatpak collection
+flatpak install -y --noninteractive flathub org.mozilla.firefox
+flatpak install -y --noninteractive flathub com.github.wwmm.easyeffects 
+flatpak install -y --noninteractive flathub io.mpv.Mpv
+flatpak install -y --noninteractive flathub org.libreoffice.LibreOffice 
+flatpak install -y --noninteractive flathub org.mozilla.Thunderbird 
+flatpak install -y --noninteractive flathub md.obsidian.Obsidian 
+flatpak install -y --noninteractive flathub com.heroicgameslauncher.hgl 
+flatpak install -y --noninteractive flathub org.prismlauncher.PrismLauncher 
+flatpak install -y --noninteractive flathub com.mattjakeman.ExtensionManager 
+flatpak install -y --noninteractive flathub com.github.tchx84.Flatseal 
+flatpak install -y --noninteractive flathub org.keepassxc.KeePassXC 
+flatpak install -y --noninteractive flathub com.rafaelmardojai.Blanket 
+flatpak install -y --noninteractive flathub com.discordapp.Discord 
+flatpak install -y --noninteractive flathub org.qbittorrent.qBittorrent 
+flatpak install -y --noninteractive flathub org.gnome.Calendar 
+flatpak install -y --noninteractive flathub org.gnome.Characters 
+flatpak install -y --noninteractive flathub org.gnome.Cheese 
+flatpak install -y --noninteractive flathub org.gnome.Loupe 
+flatpak install -y --noninteractive flathub org.gnome.Maps 
+flatpak install -y --noninteractive flathub org.gnome.font-viewer
