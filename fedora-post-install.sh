562cf2feb5eca077ea97405b184bfdcdacf1f4dd
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-03-10 12:18:07 -0300

    Update fedora-post-install.sh

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -46,7 +46,7 @@ sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
 # Personal stuff
 
 # .heic extension support (for Apple photos), Utilities, Essetials
-sudo dnf install heif-pixbuf-loader libheif-tools restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme -y
+sudo dnf install heif-pixbuf-loader libheif-tools restic neovim util-linux-user gnome-shell-extension-appindicator gnome-shell-extension-just-perfection gnome-shell-extension-gsconnect gnome-shell-extension-user-theme gnome-shell-extension-blur-my-shell gnome-shell-extension-dash-to-dock gnome-tweaks ffmpegthumbnailer google-noto-sans-runic-fonts neofetch htop strawberry torbrowser-launcher steam adw-gtk3-theme yt-dlp fish gh papirus-icon-theme gnome-shell-theme-yaru yaru-sound-theme -y
 
 # Not needed apps
 sudo dnf remove libreoffice-core rhythmbox totem gnome-extensions-app simple-scan mediawriter gnome-connections gnome-clocks gnome-contacts gnome-weather gnome-text-editor gnome-calculator gnome-tour gnome-system-monitor gnome-shell-extension-gamemode gnome-classic-session gnome-font-viewer gnome-characters loupe gnome-calendar cheese gnome-maps firefox -y
@@ -55,7 +55,7 @@ sudo dnf group remove Libreoffice -y
 # dotfiles configuration
 git clone https://github.com/MrQvest/.dotfiles.git $HOME/.dotfiles
 
-# Install Starship
+# Install Starship Prompt
 curl -sS https://starship.rs/install.sh | sh
 
 # NVIDIA Drivers
