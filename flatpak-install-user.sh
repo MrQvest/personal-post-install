eba05469bbb03f928fcda13eef0b6ae70f3f6509
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-03-17 12:46:53 -0300

    Add files via upload

---
+++ flatpak-install-user.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Install personal flatpak collection
+
+# EasyEffects
+flatpak install -y --noninteractive --user flathub com.github.wwmm.easyeffects
+# LibreOffice
+flatpak install -y --noninteractive --user flathub org.libreoffice.LibreOffice 
+# Thunderbird
+flatpak install -y --noninteractive --user flathub org.mozilla.Thunderbird 
+# Obsidian
+flatpak install -y --noninteractive --user flathub md.obsidian.Obsidian 
+# Heroic Games Launcher
+flatpak install -y --noninteractive --user flathub com.heroicgameslauncher.hgl 
+# Prism Launcher
+flatpak install -y --noninteractive --user flathub org.prismlauncher.PrismLauncher 
+# Extension Manager
+flatpak install -y --noninteractive --user flathub com.mattjakeman.ExtensionManager 
+# Flatseal
+flatpak install -y --noninteractive --user flathub com.github.tchx84.Flatseal 
+# KeePassXC
+flatpak install -y --noninteractive --user flathub org.keepassxc.KeePassXC 
+# Blanket
+flatpak install -y --noninteractive --user flathub com.rafaelmardojai.Blanket 
+# Vesktop (Discord)
+flatpak install -y --noninteractive --user flathub dev.vencord.Vesktop
+# qBittorrent
+flatpak install -y --noninteractive --user flathub org.qbittorrent.qBittorrent 
+
+# GNOME Apps
+flatpak install -y --noninteractive --user flathub org.gnome.Calendar 
+flatpak install -y --noninteractive --user flathub org.gnome.Characters 
+flatpak install -y --noninteractive --user flathub org.gnome.Cheese 
+flatpak install -y --noninteractive --user flathub org.gnome.Loupe 
+flatpak install -y --noninteractive --user flathub org.gnome.Maps 
+flatpak install -y --noninteractive --user flathub org.gnome.font-viewer
