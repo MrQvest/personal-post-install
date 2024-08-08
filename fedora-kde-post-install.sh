ad9d325adbb211914e3f88e6bd55a15e16f87140
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-08-08 20:42:12 -0300

    Update fedora-kde-post-install.sh
    Remove RPMFusion

--- fedora-kde-post-install.sh
+++ fedora-kde-post-install.sh
@@ -19,38 +19,11 @@ sudo dnf copr enable che/nerd-fonts
 echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 
-##################################################################
-# RPMFusion stuff
-# Install RPM Fusion repositories
-sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-
-# AppStream metadata
-sudo dnf groupupdate core -y
-
-# Multimedia stuff / codecs
-sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
-sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
-sudo dnf groupupdate sound-and-video -y
-
-# Install hardware accelerated codecs
-sudo dnf install intel-media-driver -y
-sudo dnf install libva-intel-driver -y
-sudo dnf install nvidia-vaapi-driver -y
-
-# DVD
-sudo dnf install rpmfusion-free-release-tainted -y
-sudo dnf install libdvdcss -y
-
-# Firmware
-sudo dnf install rpmfusion-nonfree-release-tainted -y
-sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
-##################################################################
-
 # Personal stuff
 
 # Not needed apps
 sudo dnf group remove Libreoffice -y
-sudo dnf remove mediawriter kwrite kontact kmail dragonplayer elisa-player firewall-config kamoso kmahjongg kmines kmousetool kmouth kolourpaint kpat krdc krfb ktnef akregator libreoffice-core neochat skanpage korganizer kaddressbook kfind kcharselect kgpg virtualbox-guest-additions kwalletmanager5 -y
+sudo dnf remove mediawriter kwrite kontact kmail okular dragonplayer elisa-player firewall-config kamoso kmahjongg kmines kmousetool kmouth kolourpaint kpat krdc krfb ktnef akregator libreoffice-core neochat skanpage korganizer kaddressbook kfind kcharselect kgpg virtualbox-guest-additions kwalletmanager5 -y
 
 # .heic extension support (for Apple photos), Utilities, Essetials, NVIDIA Drivers
 sudo dnf install heif-pixbuf-loader libheif-tools neovim google-noto-sans-runic-fonts fastfetch steam fish akmod-nvidia git-core yt-dlp audacious -y
