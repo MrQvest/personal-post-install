39ff12a40ca75755d4a4820d33f3a72ee584a3bf
MrQvest <104365438+MrQvest@users.noreply.github.com>
2024-09-05 20:01:14 -0300

    Update fedora-kde-post-install.sh

--- fedora-kde-post-install.sh
+++ fedora-kde-post-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Improve system responsiveness (Audio crackling is gone with this)
-#sudo grubby --args="preempt=full" --update-kernel=ALL
+sudo grubby --args="preempt=full" --update-kernel=ALL
 
 # dotfiles configuration
 git clone --bare https://github.com/MrQvest/.dotfiles $HOME/.dotfiles
@@ -19,6 +19,30 @@ sudo dnf copr enable che/nerd-fonts
 echo 'fastestmirror=True' | sudo tee -a /etc/dnf/dnf.conf
 echo 'max_parallel_downloads=10' | sudo tee -a /etc/dnf/dnf.conf
 
+# RPM Fusion
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
+
+# Appstream data
+sudo dnf group update core -y
+
+# ffmpeg
+sudo dnf swap ffmpeg-free ffmpeg --allowerasing -y
+
+# Codecs
+sudo dnf update @multimedia --setopt="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
+sudo dnf update @sound-and-video -y
+
+# Hardware Acceleration
+sudo dnf install intel-media-driver libva-intel-driver libva-nvidia-driver -y
+
+# DVD
+sudo dnf install rpmfusion-free-release-tainted -y
+sudo dnf install libdvdcss -y
+
+# Firmware
+sudo dnf install rpmfusion-nonfree-release-tainted -y
+sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
+
 # Personal stuff
 
 # Not needed apps
@@ -27,4 +51,4 @@ sudo dnf remove mediawriter kontact kmail okular dragonplayer elisa-player firew
 # .heic extension support (for Apple photos), Utilities, Essetials, NVIDIA Drivers
 sudo dnf install heif-pixbuf-loader libheif-tools neovim google-noto-sans-runic-fonts fastfetch steam fish akmod-nvidia git-core yt-dlp audacious nerd-fonts -y
 # Install cli tools with Homebrew
-/home/linuxbrew/.linuxbrew/bin/brew install gallery-dl topgrade starship
+/home/linuxbrew/.linuxbrew/bin/brew install gallery-dl
